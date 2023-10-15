d7118ec6b7cfc67a87c88b41f889c34d4952d6ee
Mohini <mohini.mehta21@pccoepune.org>
2023-10-15 15:58:38 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_1/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,17 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        // ignore: prefer_const_constructors
-        body: Center(
-          child: const Text("Welcome to the app made by Mohini"),
-        ),
-      ),
+    
+    return const MaterialApp(
+      home: HomePage()
     );
   }
 }
-
