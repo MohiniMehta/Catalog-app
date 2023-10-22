1e1849e3da80d096ca20a9e73e9f172fe09fb0b8
Mohini <mohini.mehta21@pccoepune.org>
2023-10-22 12:19:50 +0530

    Day 11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:flutter_project_1/models/catalog.dart";
 import "package:flutter_project_1/widgets/drawer.dart";
-import "package:flutter_project_1/widgets/item_widget.dart";
 import "dart:convert";
 
  class HomePage extends StatefulWidget {
@@ -19,25 +18,50 @@ class _HomePageState extends State<HomePage> {
     loadData();
   }
   loadData() async{
+    await Future.delayed(const Duration(seconds: 2));
     final catalogJson=await rootBundle.loadString("assets/files/catalog.json");
     final decodedData=jsonDecode(catalogJson);
     var productsData=decodedData["products"];
+    DatalogModel.items=List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {
+      
+    });
   }
   @override
   Widget build(BuildContext context) {
-    final dummyList=List.generate(20,(index)=>DatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: const Text('My App',style: TextStyle(color: Colors.black),),
         ),
-        body: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        // ignore: unnecessary_null_comparison
+        body:(DatalogModel.items!=null && DatalogModel.items.isNotEmpty)? 
+        GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
+          itemBuilder: (context,index){
+            final item=DatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                header: Container(decoration: const BoxDecoration(
+                  color:Colors.deepPurple,
+                 ),child: Text(item.name, style: const TextStyle(color: Colors.white),),
+                 ),
+                footer: Container(decoration: const BoxDecoration(
+                  color:Colors.black,
+                 ),child: Text(item.price.toString(), style: const TextStyle(color: Colors.white),),
+                 ),
+                child: Image.network(item.image),
+              )
+              );
+          }, 
+          itemCount: DatalogModel.length,
+         )
+         :const Center(child: CircularProgressIndicator(),),
         drawer: const MyDrawer(),
       );
   }
